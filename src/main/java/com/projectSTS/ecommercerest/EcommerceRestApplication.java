ada127a71545dcfc84a3a2a96b8e03f549d9b52c
LAPTOP-4FCSUT0V\Minsait <mickyleronesalomon@gmail.com>
2026-09-21 14:43:17 +0200

    Initial commit: Esqueleto del E-commerce

---
+++ src/main/java/com/projectSTS/ecommercerest/EcommerceRestApplication.java
@@ -0,0 +1,13 @@
+package com.projectSTS.ecommercerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceRestApplication.class, args);
+	}
+
+}
