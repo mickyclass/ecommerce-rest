eb6b6ba6a882cbdc3eafa5bbbc91ca517a042377
LAPTOP-4FCSUT0V\Minsait <mickyleronesalomon@gmail.com>
2026-09-22 16:06:56 +0200

    Generación y gestión del Bean Producto, su DTO, Respository y añadidas dependencies de pom.xml y los properties para PostGreSQL

---
+++ src/main/java/com/projectSTS/ecommercerest/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.projectSTS.ecommercerest.repository;
+
+import com.projectSTS.ecommercerest.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    // Debe estar completamente vacía por dentro
+}
